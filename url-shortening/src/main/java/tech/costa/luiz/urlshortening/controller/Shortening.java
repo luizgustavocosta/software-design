60ec56d898443ba099c9cb2d028e7f527ff087a6
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2020-09-27 20:02:11 +0200

    Add pom.xml and project url-shortening

---
+++ url-shortening/src/main/java/tech/costa/luiz/urlshortening/controller/Shortening.java
@@ -0,0 +1,7 @@
+package tech.costa.luiz.urlshortening.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Shortening {
+}
---
+++ url-shortening/src/test/java/tech/costa/luiz/urlshortening/controller/ShorteningTest.java
@@ -0,0 +1,14 @@
+package tech.costa.luiz.urlshortening.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShorteningTest {
+
+    @Test
+    void shouldHaveABody() {
+        //FIXME
+    }
+
+}
